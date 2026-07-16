b1998901aedcf35a53fe4d8aea7dacbb04bf2f78
lelkiramkeel <jameskipngetich166@gmail.com>
2026-07-16 04:02:52 -0400

    Stage 2: read endpoints with 404

--- assign1/task_manager.js
+++ assign1/task_manager.js
@@ -7,6 +7,13 @@ const hostname = '127.0.0.1';
 // middleware for parsing json
 app.use(express.json());
 
+//in-memory list of tasks
+let tasks = [
+	{ id: 1, name: 'Security audit', description: 'Go through overnight security logs', done:'True'},
+	{ id: 2, name: 'Sprint meeting', description: 'Provide overnight security report at the sprint meeting', done:'False'},
+	{ id: 3, name: 'Bruteforce alert script', description: 'Write, test and deploy a scrip to alert incase of password bruteforce attack to the system', done:'False'}
+];
+
 // GET /   api description
 app.get('/', (req, res) => {
 	res.status(200);
@@ -18,6 +25,19 @@ app.get('/', (req, res) => {
 app.get('/health', (req,res) => {
 	res.status(200);
 	res.json({"Status" : "OK"});
+});
+
+// GET /task
+app.get('/tasks', (req,res) => {
+	res.json(tasks);
+	res.status(200);
+});
+
+// GET /tasks/:id
+app.get('/tasks/:id', (req,res) => {
+	const task = tasks.find(t => t.id === parseInt(req.params.id));
+	if (!task) return res.status(404).json({ "error" : `Task ${req.params.id} N O T  F O U N D`});
+	res.json(task);
 });
 
 // start the server
